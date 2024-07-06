147a77496fbfce2cb614f286cfcb2943924b1199
Abrar Mostofa <aabrarmostofa@gmail.com>
2024-07-06 12:33:35 +0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// imgage 
+
+const img1 = 'https://i.ibb.co/J7xxhrc/abrar.jpg';
+const img2 = 'https://i.ibb.co/3kbc4Cc/abrar1.jpg';
+const img3 = 'https://i.ibb.co/5L2RBPf/abrar3.jpg';
+
+
+const img = document.getElementById('hero-img');
+img.src = img3;
+
+
+// cv 
+const cvUrl = 'https://drive.google.com/file/d/1jSH_korllznXIXOEO5UDR-aZx5yfb2yg/view'
+
+const cv = document.getElementById('cv');
+cv.setAttribute('href', cvUrl);
